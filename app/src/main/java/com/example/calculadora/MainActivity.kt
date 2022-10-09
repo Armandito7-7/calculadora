bc521f556dcaedd0c0c1dd5f5df96ae5caae0861
Armandito7-7 <105897543+Armandito7-7@users.noreply.github.com>
2022-10-09 11:22:01 -0500

    primer cambio
    la etiqueta de label ya se muestra las operaciones pero no hace la oepracion

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -8,6 +8,9 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     //0->nada, 1->suma, 2->resta, 3->multi, 4->division
+    // decicion == 1 -> no hay operacion      decision == 2 -> si hay operacion  || poder concatenar strings y comvertir el numero1 en un string
+    var acumulador: String = ""
+    var decision: Int = 0
     var oper: Int = 0
     var numero1: Double = 0.0
     lateinit var tv_num1: TextView
@@ -48,46 +51,85 @@ class MainActivity : AppCompatActivity() {
 
     fun presionarDigito(view: View){
         //val tv_num2: TextView = findViewById(R.id.tv_num2)
-        var num2: String = tv_num2.text.toString()
+        var num1: String = tv_num1.text.toString()
 
         when(view.id){
-            R.id.ceroBoton -> tv_num2.setText(num2 + "0")
-            R.id.unoBoton -> tv_num2.setText(num2 + "1")
-            R.id.dosBoton -> tv_num2.setText(num2 + "2")
-            R.id.tresBoton -> tv_num2.setText(num2 + "3")
-            R.id.cuatroBoton -> tv_num2.setText(num2 + "4")
-            R.id.cincoBoton -> tv_num2.setText(num2 + "5")
-            R.id.seisBoton -> tv_num2.setText(num2 + "6")
-            R.id.sieteBoton -> tv_num2.setText(num2 + "7")
-            R.id.ochoBoton -> tv_num2.setText(num2 + "8")
-            R.id.nueveBoton -> tv_num2.setText(num2 + "9")
-            R.id.puntoBoton -> tv_num2.setText(num2 + ".")
+            R.id.ceroBoton -> {
+                tv_num1.setText(num1 + "0")
+                acumulador = acumulador + "0"
+
+            }
+            R.id.unoBoton -> {
+                tv_num1.setText(num1 + "1")
+                acumulador = acumulador + "1"
+            }
+            R.id.dosBoton -> {
+                tv_num1.setText(num1 + "2")
+                acumulador = acumulador + "2"
+            }
+            R.id.tresBoton -> {
+                tv_num1.setText(num1 + "3")
+                acumulador = acumulador + "3"
+            }
+            R.id.cuatroBoton -> {
+                tv_num1.setText(num1 + "4")
+                acumulador = acumulador + "4"
+            }
+            R.id.cincoBoton -> {
+                tv_num1.setText(num1 + "5")
+                acumulador = acumulador + "5"
+            }
+            R.id.seisBoton -> {
+                tv_num1.setText(num1 + "6")
+                acumulador = acumulador + "6"
+            }
+            R.id.sieteBoton -> {
+                tv_num1.setText(num1 + "7")
+                acumulador = acumulador + "7"
+            }
+            R.id.ochoBoton -> {
+                tv_num1.setText(num1 + "8")
+                acumulador = acumulador + "8"
+            }
+            R.id.nueveBoton -> {
+                tv_num1.setText(num1 + "9")
+                acumulador = acumulador + "9"
+            }
+            R.id.puntoBoton -> {
+                tv_num1.setText(num1 + ".")
+                acumulador = acumulador + "."
+            }
 
         }
 
     }
 
     fun clicOperacion(view: View){
-        numero1 = tv_num2.text.toString().toDouble()
-        var num2_text: String = tv_num2.text.toString()
-        tv_num2.setText("")
+        //numero1 = tv_num2.text.toString().toDouble()
+        var num1_text: String = tv_num1.text.toString()
+        //tv_num2.setText("")
 
-        when(view.id){
-            R.id.sumaBoton ->{
-                tv_num1.setText(num2_text + "+")
-                oper = 1
+        when(view.id) {
+            R.id.sumaBoton -> {
+                tv_num1.setText(num1_text + "+")
+                numero1 = acumulador.toString().toDouble()
+                acumulador = ""
+                //oper = 1
             }
-            R.id.restaBoton ->{
-                tv_num1.setText(num2_text + "-")
-                oper = 2
+            R.id.restaBoton -> {
+                tv_num1.setText(num1_text + "-")
+                acumulador = ""
+                //oper = 2
             }
-            R.id.multiBoton ->{
-                tv_num1.setText(num2_text + "*")
-                oper = 3
+            R.id.multiBoton -> {
+                tv_num1.setText(num1_text + "*")
+                acumulador = ""
+                //oper = 3
             }
-            R.id.diviBoton ->{
-                tv_num1.setText(num2_text + "/")
-                oper = 4
+            R.id.diviBoton -> {
+                tv_num1.setText(num1_text + "/")
+                acumulador = ""
+                //oper = 4
             }
         }
     }
